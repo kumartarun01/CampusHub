aff6acf918054e4dcdeeee9a7ac613a1e5a6faee
Tarun Kumar <jaatrony84@gmail.com>
2026-04-02 16:26:23 +0530

    Initial Commit

---
+++ CampusHub/CampusHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusHubApp.swift
+//  CampusHub
+//
+//  Created by iMac1 on 02/04/26.
+//
+
+import SwiftUI
+
+@main
+struct CampusHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CampusHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CampusHub
+//
+//  Created by iMac1 on 02/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
